145954eafbba9e997d827bcf6b18ead9c4a4660e
ueltongomes <uelton.gomes@uol.com.br>
2023-08-31 19:42:38 -0300

    Atividade input

---
+++ aulaVscode/atividade.py
@@ -0,0 +1,6 @@
+nome = input('Digite o seu nome: ')
+altura = input('Digite o sua altura em metros: ')
+idade = input('Digite sua idade: ')
+
+
+print(f" Meu nome é: {nome}, minha altura é {altura} m  e minha idade é {idade} anos")
